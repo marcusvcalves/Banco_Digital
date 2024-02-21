352e1c3de5198e1b27cfadd9c3039abe0e8583b1
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2024-02-21 20:40:55 -0300

    feat: adicionados scopes, automapper, cors

--- server/API/Program.cs
+++ server/API/Program.cs
@@ -1,9 +1,45 @@
+using Infra.Configurations;
+using Infra.Data;
+using Infra.Repositories.ApoliceRepo;
+using Infra.Repositories.CartaoRepo;
+using Infra.Repositories.ClienteRepo;
+using Infra.Repositories.ContaRepo;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiDatabase"));
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IApoliceRepository, ApoliceRepository>();
+builder.Services.AddScoped<ICartaoRepository, CartaoRepository>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IContaRepository, ContaRepository>();
+
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000", "https://localhost:3000", "http://192.168.0.7:3000")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        });
+});
 
 var app = builder.Build();
 
@@ -15,5 +51,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.MapControllers();
 
 app.Run();
